55a9bc80d936ad35accb69a7d40167b6e058d60e
darevenky <darevenky96@gmail.com>
2023-03-08 23:17:18 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from app1.views import *
+
+app_name='something'
+
+urlpatterns=[
+    path('venky1/',venky1,name='venky1'),
+    path('venky2/',venky2,name='venky2'),
+]
